bffa0c4d49f2283df062207ec0d0228df9fef1a0
IzaDobrosz <izabela.dobrosz@gmail.com>
2024-04-27 10:25:44 +0200

    forms update

--- my_garden/my_garden_app/forms.py
+++ my_garden/my_garden_app/forms.py
@@ -1,4 +1,47 @@
-# from django.contrib.auth.forms import AuthenticationForm
-#
-# class LoginForm(AuthenticationForm):
-#     pass
+from django import forms
+from .models import Plant, PlantMaintenance
+
+
+class PlantAddForm(forms.ModelForm):
+    class Meta:
+        model = Plant
+        fields = (
+            "name",
+            "description",
+            "max_height",
+            "spread",
+            "flowering_season",
+            "sunlight_exposure",
+            "pruning_frequency",
+            "watering_needs",
+            "fertilization",
+            "pest_disease_resistance"
+        )
+
+
+class PlantEditForm(forms.ModelForm):
+    class Meta:
+        model = Plant
+        fields = (
+            "name",
+            "description",
+            "max_height",
+            "spread",
+            "flowering_season",
+            "sunlight_exposure",
+            "pruning_frequency",
+            "watering_needs",
+            "fertilization",
+            "pest_disease_resistance"
+        )
+
+class PlantMaintenanceForm(forms.ModelForm):
+    class Meta:
+        model = PlantMaintenance
+        fields = [
+            'plant',
+            'task',
+            'task_description',
+            'week_of_month',
+            'month'
+        ]
